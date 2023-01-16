4763856ed9fd5a9d455fc49bdb092cd7c89f17c0
Kirtimaanwani <sushilsw111@gmail.com>
2023-01-16 23:40:14 +0530

    Update

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+
+
+# -----------------------------------------------------------------
+st.set_page_config(page_title='Applied ML App',
+    layout='wide')
+
+st.write("""
+# Applied ML App
+""")
