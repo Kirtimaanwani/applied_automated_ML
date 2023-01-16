4f69455fec0760303bb79dd0722a168c252edc92
Kirtimaanwani <sushilsw111@gmail.com>
2023-01-17 00:48:44 +0530

    Update

--- Applied_ML.py
+++ Applied_ML.py
@@ -137,7 +137,7 @@ def get_card_split(df, cols, n=11):
 # Helper class for performing classification
 
 
-class LazyClassifier:
+class Classifier:
     """
     This module helps in fitting to all the classification algorithms that are available in Scikit-learn
     Parameters
@@ -156,14 +156,13 @@ class LazyClassifier:
 
     Examples
     --------
-    >>> from lazypredict.Supervised import LazyClassifier
     >>> from sklearn.datasets import load_breast_cancer
     >>> from sklearn.model_selection import train_test_split
     >>> data = load_breast_cancer()
     >>> X = data.data
     >>> y= data.target
     >>> X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.5,random_state =123)
-    >>> clf = LazyClassifier(verbose=0,ignore_warnings=True, custom_metric=None)
+    >>> clf = Classifier(verbose=0,ignore_warnings=True, custom_metric=None)
     >>> models,predictions = clf.fit(X_train, X_test, y_train, y_test)
     >>> model_dictionary = clf.provide_models(X_train,X_test,y_train,y_test)
     >>> models
@@ -419,7 +418,7 @@ def adjusted_rsquared(r2, n, p):
 # Helper class for performing classification
 
 
-class LazyRegressor:
+class Regressor:
     """
     This module helps in fitting regression models that are available in Scikit-learn
     Parameters
@@ -438,7 +437,6 @@ class LazyRegressor:
 
     Examples
     --------
-    >>> from lazypredict.Supervised import LazyRegressor
     >>> from sklearn import datasets
     >>> from sklearn.utils import shuffle
     >>> import numpy as np
@@ -451,7 +449,7 @@ class LazyRegressor:
     >>> X_train, y_train = X[:offset], y[:offset]
     >>> X_test, y_test = X[offset:], y[offset:]
 
-    >>> reg = LazyRegressor(verbose=0, ignore_warnings=False, custom_metric=None)
+    >>> reg = Regressor(verbose=0, ignore_warnings=False, custom_metric=None)
     >>> models, predictions = reg.fit(X_train, X_test, y_train, y_test)
     >>> model_dictionary = reg.provide_models(X_train, X_test, y_train, y_test)
     >>> models
@@ -689,5 +687,5 @@ class LazyRegressor:
         return self.models
 
 
-Regression = LazyRegressor
-Classification = LazyClassifier
+Regression = Regressor
+Classification = Classifier
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-
-
-
+from Applied_ML import Classifier, Regressor
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # -----------------------------------------------------------------
 st.set_page_config(page_title='Applied ML App',
@@ -10,3 +10,42 @@ st.set_page_config(page_title='Applied ML App',
 st.write("""
 # Applied ML App
 """)
+
+
+add_sidebar = st.sidebar.selectbox('Select type of problem', ('REGRESSION', 'CLASSIFICATION'))
+
+data = st.file_uploader('Upload a CSV')
+target_column_name = st.text_input('Please type Target column name here')
+
+
+if add_sidebar == 'REGRESSION':
+    if st.button("Build"):
+        if not data:
+            st.write("Please upload a csv file")
+        else:
+            if not target_column_name:
+                st.write("Please write a target column name")
+            else:
+                df = pd.read_csv(data)
+                X = df.drop(target_column_name, axis=1)
+                y = df[target_column_name]
+                X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.3,random_state = 143)
+                reg = Regressor(verbose=0, ignore_warnings=False, custom_metric=None)
+                models, predictions = reg.fit(X_train, X_test, y_train, y_test)
+                st.table(models)
+
+if add_sidebar == 'CLASSIFICATION':
+    if st.button("Build"):
+        if not data:
+            st.write("Please upload a csv file")
+        else:
+            if not target_column_name:
+                st.write("Please write a target column name")
+            else:
+                df = pd.read_csv(data)
+                X = df.drop(target_column_name, axis=1)
+                y = df[target_column_name]
+                X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.3,random_state = 143)
+                reg = Classifier(verbose=0, ignore_warnings=False, custom_metric=None)
+                models, predictions = reg.fit(X_train, X_test, y_train, y_test)
+                st.table(models)
